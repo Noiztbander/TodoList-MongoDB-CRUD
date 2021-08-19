1f844b7fe76b67bf028716d050a949ad816acd0d
Erick Noiztbander <78412093+Noiztbander@users.noreply.github.com>
2021-08-19 21:51:46 +0200

    Styling more or less Done

--- src/database.js
+++ src/database.js
@@ -1,6 +1,9 @@
 const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+dotenv.config();
+const uri = process.env.URL_ATLAS || "mongodb://localhost/notes-db-app";
 
-mongoose.connect("mongodb://localhost/notes-db-app", {
+mongoose.connect(uri, {
 	useCreateIndex: true,
 	useNewUrlParser: true,
 	useFindAndModify:false,
